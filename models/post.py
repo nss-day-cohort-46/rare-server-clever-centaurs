a4004ea9fc8aa9efdeaa3408f6473411c65b0fe4
kylieanyce <kylieanyce@gmail.com>
2021-04-28 15:52:52 -0500

    working on update method not tested in postman yet

---
+++ models/post.py
@@ -0,0 +1,12 @@
+class Post():
+
+    # Class initializer. It has custom parameters, with the
+    # special `self` parameter that every method on a class
+    # needs as the first parameter.
+    def __init__(self, id, user_id, category_id, title, publication_date, content):
+        self.id = id
+        self.user_id = user_id
+        self.category_id = category_id
+        self.title = title
+        self.publication_date = publication_date
+        self.content = content
